dbec8a595b4a5a40892e4efb1abf952fe66a35d3
Prabeen <prabeensoreng2119@gmail.com>
2019-02-06 18:11:41 +0530

    issue solved in map component

--- src/app/common/map/map.service.ts
+++ src/app/common/map/map.service.ts
@@ -43,7 +43,7 @@ export class MapService {
     public getGeoLocation(location: string): Observable<any> {
         if(this.isLocationCached(location)) {
             // return location from cache
-            return of(this.locationCache(this.camelize(location)));
+            return of(this.locationCache[this.camelize(location)]);
         } else {
             return this.geocodeLocation(location);
         }
